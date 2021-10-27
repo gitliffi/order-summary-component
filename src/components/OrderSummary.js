2b1469ed2c9de226461b50c3675d2f68a32a3195
gitliffi <muhdaliffi@gmail.com>
2021-10-27 17:06:31 +0800

    Initialized layout of order summary card
    Added and modified background image to design specifications
    Added and modified card image to design specifications
    Basic structure for card text and card buttons

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function OrderSummary() {
-    return(
-        <div>
-            Test
+  return (
+    <>
+      <div className="card">
+        <div className="card-image"></div>
+        <div className="card-body">
+          <h2 className="card-title">Order Summary</h2>
+          <p className="card-body">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
         </div>
-    )
+        <div className="card-buttons">
+          <p className="card-subscription">
+            Annual Plan $59.99/year
+            <a className="card-subscription-button" href="#">
+              Change
+            </a>
+          </p>
+          <a className="card-payment-button" href="#">
+            Proceed to Payment
+          </a>
+          <a className="card-cancel-button" href="#">
+            Cancel Order
+          </a>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
 }
 
-export default OrderSummary
+export default OrderSummary;
