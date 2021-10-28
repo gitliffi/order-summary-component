0c5466a191c49943ff91a79ae10acfe89b2a1107
gitliffi <muhdaliffi@gmail.com>
2021-10-28 11:29:35 +0800

    card title and text adjusted to design

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -6,25 +6,30 @@ function OrderSummary() {
       <div className="card">
         <div className="card-image"></div>
         <div className="card-body">
-          <h2 className="card-title">Order Summary</h2>
-          <p className="card-body">
-            You can now listen to millions of songs, audiobooks, and podcasts on
-            any device anywhere you like!
-          </p>
-        </div>
-        <div className="card-buttons">
+          <div className="card-text">
+            <h2 className="card-body-title">Order Summary</h2>
+            <p className="card-body-paragraph">
+              You can now listen to millions of songs, audiobooks, and podcasts
+              on any device anywhere you like!
+            </p>
+          </div>
           <div className="card-subscription">
+            <img src="./images/icon-music.svg"></img>
             <p className="card-subscription-text">Annual Plan $59.99/year</p>
             <a className="card-subscription-button" href="#">
               Change
             </a>
           </div>
-          <a className="card-payment-button" href="#">
-            Proceed to Payment
-          </a>
-          <a className="card-cancel-button" href="#">
-            Cancel Order
-          </a>
+          <div className="proceed">
+            <a className="card-payment-button" href="#">
+              Proceed to Payment
+            </a>
+          </div>
+          <div className="cancel">
+            <a className="card-cancel-button" href="#">
+              Cancel Order
+            </a>
+          </div>
         </div>
       </div>
       <div class="attribution">
